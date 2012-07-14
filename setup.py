398c362ea6745569a866918d913932ca1bf58f94
Francesco Occhipinti <f.occhipinti@gmail.com>
2012-07-14 14:46:19 +0200

    Removed the vishnje part from this repo

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vishnje',
+    name='tornado',
     include_package_data=True,
     packages=find_packages(),
-    install_requires='CherryPy',
+    install_requires='vishnje',
+    dependency_links=[
+        'git+https://github.com/danse/vishnje.git#egg=vishnje-dev',
+        ],
     scripts=[
         'scripts/tornado.py',
         ],
